40472d7299fd7914fdd9089ca1cce2695a6883b5
Stephen Banghart <stephen.banghart@nist.gov>
2016-08-15 14:28:34 -0400

    Validation Fixes

--- polie/polie-core/src/main/java/gov/nist/rolie/polie/core/servlet/RESTEventValidationVisitor.java
+++ polie/polie-core/src/main/java/gov/nist/rolie/polie/core/servlet/RESTEventValidationVisitor.java
@@ -48,8 +48,25 @@ public class RESTEventValidationVisitor implements RESTEventVisitor {
 
 	@Override
 	public Response visit(Put put) {
-		// TODO Auto-generated method stub
-		return null;
+		String body = put.getBody();
+		Response.ResponseBuilder rb = Response.status(200);
+		try {
+			RolieModelValidator.validate(ROLIEDocumentUtils.buildDocFromText(body));
+		} catch (ParserConfigurationException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (SAXException e) {
+			rb=Response.status(400);
+			rb.entity("XML Invalid");
+		}
+		
+		return rb.build();
 	}
 
 	@Override
