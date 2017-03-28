6a413b01bda49bc0ba374902d4ba4a38fd6aecc8
Stephen Banghart <stephen.banghart@nist.gov>
2017-03-28 16:32:34 -0400

    CLI WIP

--- polie/polie-client/src/main/java/gov/nist/rolie/polie/client/CLI.java
+++ polie/polie-client/src/main/java/gov/nist/rolie/polie/client/CLI.java
@@ -1,5 +1,19 @@
+
 package gov.nist.rolie.polie.client;
 
-public class CLI {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class CLI {
+  public static void main(String[] args) {
+    try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in))) {
+
+      String line;
+      while ((line = in.readLine()) != null) {
+        // parse(line)
+      }
+    } catch (IOException e) {
+    }
+  }
 }
